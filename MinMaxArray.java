c86bf24e0b0b6e6d09057a300e8547b254cf37fb
ANKUSH0480 <rajankush0480@gmail.com>
2026-02-08 23:46:26 +0530

    .

---
+++ MinMaxArray.java
@@ -0,0 +1,19 @@
+public class MinMaxArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, 5, 8, 6};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("[" + min + ", " + max + "]");
+    }
+}
